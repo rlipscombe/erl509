d1cdd0f2b98da591345f039f00998ff722a9ae24
Roger Lipscombe <roger@differentpla.net>
2025-03-26 19:44:18 +0000

    Reformat code

--- src/erl509_time.erl
+++ src/erl509_time.erl
@@ -5,6 +5,16 @@ encode_time(Time) ->
     encode_universal_time(calendar:system_time_to_universal_time(Time, seconds)).
 
 encode_universal_time({{Ye, Mo, Da}, {Ho, Mi, Se}}) when Ye < 2050 ->
-    {utcTime, iolist_to_binary(io_lib:format("~2.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0BZ", [Ye rem 100, Mo, Da, Ho, Mi, Se]))};
+    {utcTime,
+        iolist_to_binary(
+            io_lib:format("~2.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0BZ", [
+                Ye rem 100, Mo, Da, Ho, Mi, Se
+            ])
+        )};
 encode_universal_time({{Ye, Mo, Da}, {Ho, Mi, Se}}) when Ye >= 2050 ->
-    {generalTime, iolist_to_binary(io_lib:format("~4.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0BZ", [Ye, Mo, Da, Ho, Mi, Se]))}.
+    {generalTime,
+        iolist_to_binary(
+            io_lib:format("~4.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0B~2.10.0BZ", [
+                Ye, Mo, Da, Ho, Mi, Se
+            ])
+        )}.
