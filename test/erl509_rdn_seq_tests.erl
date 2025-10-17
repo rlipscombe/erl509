05f0eb48bd54df3f2fa4e1dd63a0901df8eb7c19
Roger Lipscombe <roger@differentpla.net>
2025-10-17 10:48:36 +0100

    wip

--- test/erl509_rdn_seq_tests.erl
+++ test/erl509_rdn_seq_tests.erl
@@ -6,8 +6,7 @@ cn_test() ->
     ?assertEqual(
         {rdnSequence, [
             [
-                {'AttributeTypeAndValue', ?'id-at-commonName',
-                    public_key:der_encode('DirectoryString', {printableString, "example"})}
+                {'AttributeTypeAndValue', ?'id-at-commonName', {printableString, <<"example">>}}
             ]
         ]},
         erl509_rdn_seq:create("CN=example")
@@ -18,15 +17,11 @@ docker_desktop_test() ->
         {rdnSequence, [
             [
                 {'AttributeTypeAndValue', ?'id-at-organizationName',
-                    public_key:der_encode(
-                        'DirectoryString', {printableString, "system:masters"}
-                    )}
+                    {printableString, <<"system:masters">>}}
             ],
             [
                 {'AttributeTypeAndValue', ?'id-at-commonName',
-                    public_key:der_encode(
-                        'DirectoryString', {printableString, "docker-for-desktop"}
-                    )}
+                    {printableString, <<"docker-for-desktop">>}}
             ]
         ]},
         erl509_rdn_seq:create(<<"O=system:masters, CN=docker-for-desktop">>)
