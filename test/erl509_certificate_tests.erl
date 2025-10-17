64ce34458d621f8d189ee74e9d103ae97ecc50df
Roger Lipscombe <roger@differentpla.net>
2025-10-17 12:04:56 +0100

    undo most of this

--- test/erl509_certificate_tests.erl
+++ test/erl509_certificate_tests.erl
@@ -12,9 +12,7 @@ self_signed_rsa_test() ->
     % ?assertEqual(Certificate, erl509_certificate:from_pem(PEM)),
     _DER = erl509_certificate:to_der(Certificate),
 
-    OTPCertificate = to_otp(Certificate),
-
-    ?assert(pubkey_cert:is_self_signed(OTPCertificate)),
+    ?assert(pubkey_cert:is_self_signed(Certificate)),
 
     #'OTPCertificate'{
         tbsCertificate = #'OTPTBSCertificate'{
@@ -31,7 +29,7 @@ self_signed_rsa_test() ->
         },
         signatureAlgorithm = SignatureAlgorithm,
         signature = Signature
-    } = OTPCertificate,
+    } = Certificate,
 
     ?assertEqual(2048, 8 * byte_size(Signature)),
 
@@ -62,181 +60,181 @@ self_signed_rsa_test() ->
     ?assertEqual(RSAPublicKey, SubjectPublicKey),
     ok.
 
-self_signed_ec_test() ->
-    ECPrivateKey = erl509_private_key:create_ec(secp256r1),
-    ECPublicKey = erl509_private_key:derive_public_key(ECPrivateKey),
-    Certificate = erl509_certificate:create_self_signed(
-        ECPrivateKey, <<"CN=example">>, erl509_certificate_template:root_ca()
-    ),
-    PEM = erl509_certificate:to_pem(Certificate),
-    ?assertEqual(Certificate, erl509_certificate:from_pem(PEM)),
+% self_signed_ec_test() ->
+%     ECPrivateKey = erl509_private_key:create_ec(secp256r1),
+%     ECPublicKey = erl509_private_key:derive_public_key(ECPrivateKey),
+%     Certificate = erl509_certificate:create_self_signed(
+%         ECPrivateKey, <<"CN=example">>, erl509_certificate_template:root_ca()
+%     ),
+%     PEM = erl509_certificate:to_pem(Certificate),
+%     ?assertEqual(Certificate, erl509_certificate:from_pem(PEM)),
 
-    OTPCertificate = to_otp(Certificate),
+%     OTPCertificate = to_otp(Certificate),
 
-    ?assert(pubkey_cert:is_self_signed(OTPCertificate)),
+%     ?assert(pubkey_cert:is_self_signed(OTPCertificate)),
 
-    #'OTPCertificate'{
-        tbsCertificate = #'OTPTBSCertificate'{
-            version = _,
-            serialNumber = _,
-            signature = SignatureAlgorithm,
-            issuer = Issuer,
-            validity = _,
-            subject = Subject,
-            subjectPublicKeyInfo = SubjectPublicKeyInfo,
-            issuerUniqueID = _,
-            subjectUniqueID = _,
-            extensions = Extensions
-        },
-        signatureAlgorithm = SignatureAlgorithm,
-        signature = _
-    } = OTPCertificate,
-    ?assertEqual(
-        {rdnSequence, [
-            [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "example"}}]
-        ]},
-        Issuer
-    ),
-    ?assertEqual(
-        {rdnSequence, [
-            [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "example"}}]
-        ]},
-        Subject
-    ),
-    ?assertEqual(
-        #'SignatureAlgorithm'{algorithm = ?'ecdsa-with-SHA256', parameters = asn1_NOVALUE},
-        SignatureAlgorithm
-    ),
-    #'OTPSubjectPublicKeyInfo'{
-        algorithm = Algorithm,
-        subjectPublicKey = SubjectPublicKey
-    } = SubjectPublicKeyInfo,
-    ?assertEqual(
-        #'PublicKeyAlgorithm'{algorithm = ?'id-ecPublicKey', parameters = {namedCurve, ?secp256r1}},
-        Algorithm
-    ),
-    {ECPoint, _} = ECPublicKey,
-    ?assertEqual(ECPoint, SubjectPublicKey),
+%     #'OTPCertificate'{
+%         tbsCertificate = #'OTPTBSCertificate'{
+%             version = _,
+%             serialNumber = _,
+%             signature = SignatureAlgorithm,
+%             issuer = Issuer,
+%             validity = _,
+%             subject = Subject,
+%             subjectPublicKeyInfo = SubjectPublicKeyInfo,
+%             issuerUniqueID = _,
+%             subjectUniqueID = _,
+%             extensions = Extensions
+%         },
+%         signatureAlgorithm = SignatureAlgorithm,
+%         signature = _
+%     } = OTPCertificate,
+%     ?assertEqual(
+%         {rdnSequence, [
+%             [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "example"}}]
+%         ]},
+%         Issuer
+%     ),
+%     ?assertEqual(
+%         {rdnSequence, [
+%             [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "example"}}]
+%         ]},
+%         Subject
+%     ),
+%     ?assertEqual(
+%         #'SignatureAlgorithm'{algorithm = ?'ecdsa-with-SHA256', parameters = asn1_NOVALUE},
+%         SignatureAlgorithm
+%     ),
+%     #'OTPSubjectPublicKeyInfo'{
+%         algorithm = Algorithm,
+%         subjectPublicKey = SubjectPublicKey
+%     } = SubjectPublicKeyInfo,
+%     ?assertEqual(
+%         #'PublicKeyAlgorithm'{algorithm = ?'id-ecPublicKey', parameters = {namedCurve, ?secp256r1}},
+%         Algorithm
+%     ),
+%     {ECPoint, _} = ECPublicKey,
+%     ?assertEqual(ECPoint, SubjectPublicKey),
 
-    % TODO: Assert all of the extensions.
-    ?assertEqual(3, length(Extensions)),
-    {value, Extn} = lists:search(
-        fun(#'Extension'{extnID = ExtnID}) -> ExtnID =:= ?'id-ce-basicConstraints' end, Extensions
-    ),
-    ?assertMatch(
-        #'Extension'{
-            critical = true, extnValue = #'BasicConstraints'{cA = true, pathLenConstraint = _}
-        },
-        Extn
-    ),
-    ok.
+%     % TODO: Assert all of the extensions.
+%     ?assertEqual(3, length(Extensions)),
+%     {value, Extn} = lists:search(
+%         fun(#'Extension'{extnID = ExtnID}) -> ExtnID =:= ?'id-ce-basicConstraints' end, Extensions
+%     ),
+%     ?assertMatch(
+%         #'Extension'{
+%             critical = true, extnValue = #'BasicConstraints'{cA = true, pathLenConstraint = _}
+%         },
+%         Extn
+%     ),
+%     ok.
 
-serial_number_test() ->
-    ECPrivateKey = erl509_private_key:create_ec(secp256r1),
-    Certificate = erl509_certificate:create_self_signed(ECPrivateKey, <<"CN=example">>, #{
-        serial_number => 12345,
-        extensions => #{}
-    }),
+% serial_number_test() ->
+%     ECPrivateKey = erl509_private_key:create_ec(secp256r1),
+%     Certificate = erl509_certificate:create_self_signed(ECPrivateKey, <<"CN=example">>, #{
+%         serial_number => 12345,
+%         extensions => #{}
+%     }),
 
-    OTPCertificate = to_otp(Certificate),
+%     OTPCertificate = to_otp(Certificate),
 
-    #'OTPCertificate'{
-        tbsCertificate = #'OTPTBSCertificate'{
-            serialNumber = SerialNumber
-        }
-    } = OTPCertificate,
-    ?assertEqual(12345, SerialNumber),
-    ok.
+%     #'OTPCertificate'{
+%         tbsCertificate = #'OTPTBSCertificate'{
+%             serialNumber = SerialNumber
+%         }
+%     } = OTPCertificate,
+%     ?assertEqual(12345, SerialNumber),
+%     ok.
 
-validity_test() ->
-    ECPrivateKey = erl509_private_key:create_ec(secp256r1),
-    Certificate = erl509_certificate:create_self_signed(ECPrivateKey, <<"CN=example">>, #{
-        % in days
-        validity => 90,
-        extensions => #{}
-    }),
+% validity_test() ->
+%     ECPrivateKey = erl509_private_key:create_ec(secp256r1),
+%     Certificate = erl509_certificate:create_self_signed(ECPrivateKey, <<"CN=example">>, #{
+%         % in days
+%         validity => 90,
+%         extensions => #{}
+%     }),
 
-    OTPCertificate = to_otp(Certificate),
+%     OTPCertificate = to_otp(Certificate),
 
-    #'OTPCertificate'{
-        tbsCertificate = #'OTPTBSCertificate'{
-            validity = Validity
-        }
-    } = OTPCertificate,
+%     #'OTPCertificate'{
+%         tbsCertificate = #'OTPTBSCertificate'{
+%             validity = Validity
+%         }
+%     } = OTPCertificate,
 
-    {NotBefore, NotAfter} = parse_validity(Validity),
-    ?assert(NotBefore =< erlang:system_time(second)),
-    ?assertEqual(90 * 24 * 60 * 60, NotAfter - NotBefore),
-    ok.
+%     {NotBefore, NotAfter} = parse_validity(Validity),
+%     ?assert(NotBefore =< erlang:system_time(second)),
+%     ?assertEqual(90 * 24 * 60 * 60, NotAfter - NotBefore),
+%     ok.
 
-server_rsa_test() ->
-    CAKey = erl509_private_key:create_rsa(2048),
-    CACert = erl509_certificate:create_self_signed(
-        CAKey, <<"CN=ca">>, erl509_certificate_template:root_ca()
-    ),
+% server_rsa_test() ->
+%     CAKey = erl509_private_key:create_rsa(2048),
+%     CACert = erl509_certificate:create_self_signed(
+%         CAKey, <<"CN=ca">>, erl509_certificate_template:root_ca()
+%     ),
 
-    ServerKey = erl509_private_key:create_rsa(2048),
-    ServerPub = erl509_public_key:derive_public_key(ServerKey),
-    ServerCert = erl509_certificate:create(
-        ServerPub, <<"CN=server">>, CACert, CAKey, erl509_certificate_template:server()
-    ),
+%     ServerKey = erl509_private_key:create_rsa(2048),
+%     ServerPub = erl509_public_key:derive_public_key(ServerKey),
+%     ServerCert = erl509_certificate:create(
+%         ServerPub, <<"CN=server">>, CACert, CAKey, erl509_certificate_template:server()
+%     ),
 
-    OTPCertificate = to_otp(ServerCert),
+%     OTPCertificate = to_otp(ServerCert),
 
-    #'OTPCertificate'{
-        tbsCertificate = #'OTPTBSCertificate'{
-            version = _,
-            serialNumber = _,
-            signature = SignatureAlgorithm,
-            issuer = Issuer,
-            validity = Validity,
-            subject = Subject,
-            subjectPublicKeyInfo = SubjectPublicKeyInfo,
-            issuerUniqueID = _,
-            subjectUniqueID = _,
-            extensions = Extensions
-        },
-        signatureAlgorithm = SignatureAlgorithm,
-        signature = _
-    } = OTPCertificate,
-    ?assertEqual(
-        {rdnSequence, [
-            [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "ca"}}]
-        ]},
-        Issuer
-    ),
-    ?assertEqual(
-        {rdnSequence, [
-            [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "server"}}]
-        ]},
-        Subject
-    ),
-    ?assertEqual(
-        #'SignatureAlgorithm'{algorithm = ?sha256WithRSAEncryption, parameters = 'NULL'},
-        SignatureAlgorithm
-    ),
-    #'OTPSubjectPublicKeyInfo'{
-        algorithm = Algorithm,
-        subjectPublicKey = SubjectPublicKey
-    } = SubjectPublicKeyInfo,
-    ?assertEqual(
-        #'PublicKeyAlgorithm'{algorithm = ?rsaEncryption, parameters = 'NULL'},
-        Algorithm
-    ),
-    ?assertEqual(ServerPub, SubjectPublicKey),
+%     #'OTPCertificate'{
+%         tbsCertificate = #'OTPTBSCertificate'{
+%             version = _,
+%             serialNumber = _,
+%             signature = SignatureAlgorithm,
+%             issuer = Issuer,
+%             validity = Validity,
+%             subject = Subject,
+%             subjectPublicKeyInfo = SubjectPublicKeyInfo,
+%             issuerUniqueID = _,
+%             subjectUniqueID = _,
+%             extensions = Extensions
+%         },
+%         signatureAlgorithm = SignatureAlgorithm,
+%         signature = _
+%     } = OTPCertificate,
+%     ?assertEqual(
+%         {rdnSequence, [
+%             [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "ca"}}]
+%         ]},
+%         Issuer
+%     ),
+%     ?assertEqual(
+%         {rdnSequence, [
+%             [{'AttributeTypeAndValue', ?'id-at-commonName', {printableString, "server"}}]
+%         ]},
+%         Subject
+%     ),
+%     ?assertEqual(
+%         #'SignatureAlgorithm'{algorithm = ?sha256WithRSAEncryption, parameters = 'NULL'},
+%         SignatureAlgorithm
+%     ),
+%     #'OTPSubjectPublicKeyInfo'{
+%         algorithm = Algorithm,
+%         subjectPublicKey = SubjectPublicKey
+%     } = SubjectPublicKeyInfo,
+%     ?assertEqual(
+%         #'PublicKeyAlgorithm'{algorithm = ?rsaEncryption, parameters = 'NULL'},
+%         Algorithm
+%     ),
+%     ?assertEqual(ServerPub, SubjectPublicKey),
 
-    {NotBefore, NotAfter} = parse_validity(Validity),
-    ?assert(NotBefore =< erlang:system_time(second)),
-    ?assertEqual(1 * 365 * 24 * 60 * 60, NotAfter - NotBefore),
+%     {NotBefore, NotAfter} = parse_validity(Validity),
+%     ?assert(NotBefore =< erlang:system_time(second)),
+%     ?assertEqual(1 * 365 * 24 * 60 * 60, NotAfter - NotBefore),
 
-    ?assertEqual(5, length(Extensions)),
-    % lists:search(fun(moo) -> false end, Extensions),
+%     ?assertEqual(5, length(Extensions)),
+%     % lists:search(fun(moo) -> false end, Extensions),
 
-    ok.
+%     ok.
 
-to_otp(#'Certificate'{} = Certificate) ->
-    DER = public_key:der_encode('Certificate', Certificate),
-    public_key:pkix_decode_cert(DER, otp).
+% to_otp(#'Certificate'{} = Certificate) ->
+%     DER = public_key:der_encode('Certificate', Certificate),
+%     public_key:pkix_decode_cert(DER, otp).
 
 parse_validity(#'Validity'{notBefore = NotBefore, notAfter = NotAfter}) ->
     {erl509_time:decode_time(NotBefore), erl509_time:decode_time(NotAfter)}.
