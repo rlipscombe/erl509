4e05ae67b4a1eb7028d47791bb9378cc5b5f9b80
Roger Lipscombe <roger@differentpla.net>
2025-10-18 12:43:14 +0100

    Tidy up public key tests

--- test/erl509_public_key_tests.erl
+++ test/erl509_public_key_tests.erl
@@ -1,21 +1,41 @@
 -module(erl509_public_key_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-rsa_test() ->
-    RSAPrivateKey = erl509_private_key:create_rsa(2048),
-    RSAPublicKey = erl509_public_key:derive_public_key(RSAPrivateKey),
-    PEM = erl509_public_key:to_pem(RSAPublicKey),
-    ?assertMatch(<<"-----BEGIN RSA PUBLIC KEY-----\n", _Rest/binary>>, PEM),
-    WrappedPEM = erl509_public_key:to_pem(RSAPublicKey, [wrap]),
-    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, WrappedPEM),
-    ok.
+rsa_test_() ->
+    {setup,
+        fun() ->
+            Key = erl509_private_key:create_rsa(2048),
+            erl509_public_key:derive_public_key(Key)
+        end,
+        {with, [
+            fun rsa_to_pem/1,
+            fun rsa_to_wrapped_pem/1
+        ]}}.
 
-ec_test() ->
-    ECPrivateKey = erl509_private_key:create_ec(secp256r1),
-    ECPublicKey = erl509_public_key:derive_public_key(ECPrivateKey),
+rsa_to_pem(Pub) ->
+    PEM = erl509_public_key:to_pem(Pub),
+    ?assertMatch(<<"-----BEGIN RSA PUBLIC KEY-----\n", _Rest/binary>>, PEM).
+
+rsa_to_wrapped_pem(Pub) ->
+    PEM = erl509_public_key:to_pem(Pub, [wrap]),
+    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, PEM).
+
+ec_test_() ->
+    {setup,
+        fun() ->
+            Key = erl509_private_key:create_ec(secp256r1),
+            erl509_public_key:derive_public_key(Key)
+        end,
+        {with, [
+            fun ec_to_pem/1,
+            fun ec_to_wrapped_pem/1
+        ]}}.
+
+ec_to_pem(Pub) ->
     % EC public keys are always wrapped.
-    PEM = erl509_public_key:to_pem(ECPublicKey),
-    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, PEM),
-    WrappedPEM = erl509_public_key:to_pem(ECPublicKey, [wrap]),
-    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, WrappedPEM),
-    ok.
+    PEM = erl509_public_key:to_pem(Pub),
+    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, PEM).
+
+ec_to_wrapped_pem(Pub) ->
+    PEM = erl509_public_key:to_pem(Pub, [wrap]),
+    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, PEM).
