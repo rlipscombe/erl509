20eadf4fe8ef0252a989f1077c9722b4626c3cdb
Roger Lipscombe <roger@differentpla.net>
2025-10-18 18:09:18 +0100

    Split up erl509_public_key_tests

--- test/erl509_public_key_ec_tests.erl
+++ test/erl509_public_key_ec_tests.erl
@@ -1,24 +1,5 @@
--module(erl509_public_key_tests).
+-module(erl509_public_key_ec_tests).
 -include_lib("eunit/include/eunit.hrl").
-
-rsa_test_() ->
-    {setup,
-        fun() ->
-            Key = erl509_private_key:create_rsa(2048),
-            erl509_public_key:derive_public_key(Key)
-        end,
-        {with, [
-            fun rsa_to_pem/1,
-            fun rsa_to_wrapped_pem/1
-        ]}}.
-
-rsa_to_pem(Pub) ->
-    PEM = erl509_public_key:to_pem(Pub),
-    ?assertMatch(<<"-----BEGIN RSA PUBLIC KEY-----\n", _Rest/binary>>, PEM).
-
-rsa_to_wrapped_pem(Pub) ->
-    PEM = erl509_public_key:to_pem(Pub, [wrap]),
-    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, PEM).
 
 ec_test_() ->
     {setup,
---
+++ test/erl509_public_key_rsa_tests.erl
@@ -0,0 +1,21 @@
+-module(erl509_public_key_rsa_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+rsa_test_() ->
+    {setup,
+        fun() ->
+            Key = erl509_private_key:create_rsa(2048),
+            erl509_public_key:derive_public_key(Key)
+        end,
+        {with, [
+            fun rsa_to_pem/1,
+            fun rsa_to_wrapped_pem/1
+        ]}}.
+
+rsa_to_pem(Pub) ->
+    PEM = erl509_public_key:to_pem(Pub),
+    ?assertMatch(<<"-----BEGIN RSA PUBLIC KEY-----\n", _Rest/binary>>, PEM).
+
+rsa_to_wrapped_pem(Pub) ->
+    PEM = erl509_public_key:to_pem(Pub, [wrap]),
+    ?assertMatch(<<"-----BEGIN PUBLIC KEY-----\n", _Rest/binary>>, PEM).
