7bea4f3a83406fb3894b94660c98e89ea4a71b1d
Roger Lipscombe <roger@differentpla.net>
2026-05-19 11:22:27 +0100

    wip: csr

--- src/erl509_rdn.erl
+++ src/erl509_rdn.erl
@@ -4,14 +4,13 @@
 -include_lib("public_key/include/public_key.hrl").
 
 create_rdn(Value) when is_binary(Value) ->
-    % TODO: Why list_to_tuple?
-    create_rdn_attr(list_to_tuple(string:split(Value, <<"=">>)));
+    create_rdn_attr(string:split(Value, <<"=">>));
 create_rdn(Value) when is_list(Value) ->
     create_rdn(list_to_binary(Value)).
 
-create_rdn_attr({<<"CN">>, Value}) when byte_size(Value) =< 256 ->
+create_rdn_attr([<<"CN">>, Value]) when byte_size(Value) =< 256 ->
     [#'AttributeTypeAndValue'{type = ?'id-at-commonName', value = {utf8String, Value}}];
-create_rdn_attr({<<"O">>, Value}) when byte_size(Value) =< 256 ->
+create_rdn_attr([<<"O">>, Value]) when byte_size(Value) =< 256 ->
     [#'AttributeTypeAndValue'{type = ?'id-at-organizationName', value = {utf8String, Value}}];
-create_rdn_attr({<<"C">>, Value}) ->
+create_rdn_attr([<<"C">>, Value]) ->
     [#'AttributeTypeAndValue'{type = ?'id-at-countryName', value = binary_to_list(Value)}].
--- src/erl509_rdn_seq.erl
+++ src/erl509_rdn_seq.erl
@@ -2,18 +2,15 @@
 -export([create/1]).
 
 create(<<"/", From/binary>>) ->
-    {rdnSequence,
-        lists:map(
-            fun(Part) ->
-                erl509_rdn:create_rdn(string:trim(Part))
-            end,
-            string:split(From, "/", all)
-        )};
+    from_list(string:split(From, "/", all));
 create(From) ->
+    from_list(string:split(From, ",", all)).
+
+from_list(Parts) ->
     {rdnSequence,
         lists:map(
             fun(Part) ->
                 erl509_rdn:create_rdn(string:trim(Part))
             end,
-            string:split(From, ",", all)
+            Parts
         )}.
